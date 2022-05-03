4e7291ee99a4b086a2271388587294e9a8bb3721
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-03 11:28:50 -0700

    load state added

--- src/views/CharacterList.js
+++ src/views/CharacterList.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCharacters = async () => {
       const res = await fetch(`https://rickandmortyapi.com/api/character/`);
       const results = await res.json();
       setCharacters(results.results);
+      setLoading(false);
     };
     fetchCharacters();
   }, []);
@@ -16,11 +18,15 @@ export default function CharacterList() {
   return (
     <>
       <div>CharacterList</div>
-      <ul>
-        {characters.map((character) => (
-          <li key={character.id}>{character.name}</li>
-        ))}
-      </ul>
+      {loading ? (
+        <label>Loading...</label>
+      ) : (
+        <ul>
+          {characters.map((character) => (
+            <li key={character.id}>{character.name}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
